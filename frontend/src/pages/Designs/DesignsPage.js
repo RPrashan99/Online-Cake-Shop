c4555be88ed666bfb594219b1c7c8085aa89475e
RPrashan99 <RashmithaMK@gmail.com>
2024-06-28 14:54:54 +0530

    first run

--- frontend/src/pages/Designs/DesignsPage.js
+++ frontend/src/pages/Designs/DesignsPage.js
@@ -47,7 +47,7 @@ export default function DesignsPage() {
 
           </div>
           <div className={classes.content}>
-             Welcome to MD Cakes!
+             Welcome to MD cakes!
           </div>
 
         </div>
